9a8e9708187a72f28b1020d2a0a75ad5d7f3fdc8
VinhNguyenUte <valtriek99@gmail.com>
2025-02-27 16:53:43 +0700

    lan 2

--- BT1_CCB/BT1_CCB/Program.cs
+++ BT1_CCB/BT1_CCB/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    // Hàm tính tiền sau thuế với optional parameter
+    static double TinhTienSauThue(double gia, double thue = 0.1)
+    {
+        return gia + (gia * thue);
+    }
+
+    private static void Main(string[] args)
+    {
+        double bien_223 = 100000; // Giá sản phẩm: 100,000 VND
+
+        // Gọi hàm với mức thuế mặc định (10%)
+        double tongTien1 = TinhTienSauThue(bien_223);
+        Console.WriteLine($"Tổng tiền sau thuế (mặc định 10%): {tongTien1} VND");
+
+        // Gọi hàm với mức thuế 5%
+        double tongTien2 = TinhTienSauThue(bien_223, 0.05);
+        Console.WriteLine($"Tổng tiền sau thuế (thuế 5%): {tongTien2} VND");
+    }
+}
--- BT1_CCB/BT1_CCB/obj/Debug/net8.0/BT1_CCB.AssemblyInfo.cs
+++ BT1_CCB/BT1_CCB/obj/Debug/net8.0/BT1_CCB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BT1_CCB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2282d81838d6852589c7e6fd531ba65b08af328")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+594d6432fcc753c4a36a96cd256fd66d706deff7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BT1_CCB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BT1_CCB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
