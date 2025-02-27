1072cf4321fd59fcac8946e1d8140ed8b96c5ea8
VinhNguyenUte <valtriek99@gmail.com>
2025-02-27 16:54:25 +0700

    lan 2

--- BT2_CCB/BT2_CCB/Program.cs
+++ BT2_CCB/BT2_CCB/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+enum OrderStatus
+{
+    Pending = 1,    // Chờ xử lý
+    Processing,     // Đang xử lý
+    Shipped,        // Đã giao hàng
+    Delivered       // Đã nhận hàng
+}
+internal class Program
+{
+    
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Danh sách trạng thái đơn hàng:");
+        foreach (string name in Enum.GetNames(typeof(OrderStatus)))
+        {
+            Console.WriteLine($"- {name} ({(int)Enum.Parse(typeof(OrderStatus), name)})");
+        }
+
+        Console.Write("\nNhập số trạng thái đơn hàng (1-4): ");
+        if (int.TryParse(Console.ReadLine(), out int statusNumber))
+        {
+            if (Enum.IsDefined(typeof(OrderStatus), statusNumber))
+            {
+                OrderStatus bien_223 = (OrderStatus)statusNumber;
+                Console.WriteLine($"Trạng thái đơn hàng: {bien_223}");
+            }
+            else
+            {
+                Console.WriteLine("Lỗi: Số nhập vào không hợp lệ!");
+            }
+        }
+    }
+}
--- BT2_CCB/BT2_CCB/obj/Debug/net8.0/BT2_CCB.AssemblyInfo.cs
+++ BT2_CCB/BT2_CCB/obj/Debug/net8.0/BT2_CCB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BT2_CCB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2282d81838d6852589c7e6fd531ba65b08af328")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b8b6fdb8b845b12e6b2a6f1498eb306b82df2eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("BT2_CCB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BT2_CCB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
