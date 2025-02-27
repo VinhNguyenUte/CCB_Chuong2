5e8c3fbb114d78c7b956350c7821ba02e91e5cca
VinhNguyenUte <valtriek99@gmail.com>
2025-02-27 16:55:46 +0700

    lan 2

--- BT5_CCB/BT5_CCB/Program.cs
+++ BT5_CCB/BT5_CCB/Program.cs
@@ -1,2 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Drawing;
+
+internal class Program
+{
+    enum PointColor
+    {
+        LightBlue,
+        BloodRed,
+        Gold
+    }
+
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public PointColor Color { get; set; }
+        public Point (int x, int y, PointColor color)
+        {
+            X = x;
+            Y = y;
+            Color = color;
+        }
+
+        public void PrintPointInfo(bool showColor = true)
+        {
+            Console.WriteLine("Toạ độ X: " + X);
+            Console.WriteLine("Toạ độ Y: " + Y);
+            if (showColor)
+            {
+                Console.WriteLine("Màu: " + Color);
+            }
+        }
+    }
+
+    class Rectangle
+    {
+        public Point ul { get; set; }
+        public Point br { get; set; }
+
+        public PointColor rcolor { get; set; }
+
+        public Rectangle (Point ul, Point br, PointColor rcolor)
+        {
+            this.ul = ul;
+            this.br = br;
+            this.rcolor = rcolor;
+        }
+
+        public void PrintRectangleInfo(bool showColor = true)
+        {
+            Console.WriteLine("Toạ độ upper-left : " + ul.X+" , "+ul.Y);
+            Console.WriteLine("Toạ độ bottom-right: " + br.X + " , "+br.Y);
+            if (showColor)
+            {
+                Console.WriteLine("Màu: " + rcolor);
+            }
+        }
+    } 
+    private static void Main(string[] args)
+    {
+        Point O = new Point(2, 1, PointColor.LightBlue);
+
+        O.PrintPointInfo();
+        O.PrintPointInfo(false);
+
+        Point A = new Point(6, 3, PointColor.LightBlue);
+
+        Rectangle bien_223 = new Rectangle(O, A, PointColor.Gold);
+        bien_223.PrintRectangleInfo();
+        bien_223.PrintRectangleInfo(false);
+    }
+}
--- BT5_CCB/BT5_CCB/obj/Debug/net8.0/BT5_CCB.AssemblyInfo.cs
+++ BT5_CCB/BT5_CCB/obj/Debug/net8.0/BT5_CCB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BT5_CCB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2282d81838d6852589c7e6fd531ba65b08af328")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6627bb064d60ae366bae3fc65264d49c9e13b0ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("BT5_CCB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BT5_CCB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
